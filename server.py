601b1d66c8f0d0f3d6306bc33c7a41783cf93ac1
Matthew Gregg <mgregg99@byu.edu>
2022-07-13 19:30:49 -0600

    wip

--- server.py
+++ server.py
@@ -1,16 +1,36 @@
-from flask import Flask
+import random
+from flask import Flask, render_template
 import json
 
 app = Flask(__name__)
 
+keys = []
+
 @app.route('/')
 @app.route('/about')
+def about():
+    return render_template('info.html')
 
 @app.route("/game")
 def game():
-    return 'dev'
+    return str(len(keys))
 
 @app.route("/ready")
+def ready():
+    if len(keys)  < 2:
+        key = str(random.random())
+        keys.append(key)
+        if len(keys) == 1:
+            return json.dumps({"key": key, "con": 'game', "color": 'red'})
+        elif len(keys) == 2:
+            return json.dumps({"key": key, "con": 'game', "color": 'yellow'})
+    else:
+        return "full"
+
+@app.route("/clear")
+def clear():
+    keys.clear()
+    return 'done'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=3000)
